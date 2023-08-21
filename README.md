0a5264096f65176f327f6b591dba8781ea897e85
Alex <rubashanov2001@gmail.com>
2023-08-21 20:10:59 +0300

    Added Type hints & awesome documentation :D

--- README.md
+++ README.md
@@ -1,8 +1,73 @@
 This is the *Sorting Tool (Python)* project I made myself.
 
+<p>In the modern world, data has become so abundant that processing it is no easy business. How can anyone make sense of all those words and numbers? In this project, you will write a program that processes textual and numeric data and sorts it. Your program will be able to determine the largest or most frequent pieces of data and perform the necessary calculations on them. Data is waiting to be sorted!</p>
 
-<p>In the modern world, data has become so abundant that processing it is no easy business. How can anyone make sense of all those words and numbers? In this project, you will write a program that processes textual and numeric data and sorts it. Your program will be able to determine the largest or most frequent pieces of data and perform the necessary calculations on them. Data is waiting to be sorted!</p><br/><br/>
+---
+## Sorting Tool
 
+A command-line tool for sorting data of various types and with different sorting criteria. 
+
+### Features
+- **Data Types**: Supports sorting lines (`line`), words (`word`), and numbers (`long`).
+- **Sorting Types**: Allows natural sorting (`natural`) or sorting by count (`byCount`).
+- **Input/Output Files**: Can read data from a specified input file and write sorted results to an output file.
+
+### Usage
+```bash
+python sorting_tool.py [-h] [-dataType {long,line,word}] [-sortingType {byCount,natural}] [-inputFile INPUTFILE] [-outputFile OUTPUTFILE]
+```
+
+### Options
+
+#### `-dataType {long,line,word}`
+- Specifies the type of data to be sorted.
+- Acceptable values: `long`, `line`, `word`.
+- Default: `word`.
+
+#### `-sortingType {byCount,natural}`
+- Specifies the sorting method.
+- Acceptable values: `byCount`, `natural`.
+- Default: Natural sorting.
+
+#### `-inputFile INPUTFILE`
+- Path to an input file containing the data to be sorted.
+- If not provided, the tool will read from the standard input.
+
+#### `-outputFile OUTPUTFILE`
+- Path to a file where sorted results will be written.
+- If not provided, the tool will print to the standard output.
+
+### Sorting Types Explained
+
+#### Natural Sorting
+- Sorts data in their natural order (alphanumerically).
+- For numbers, sorts them in ascending order.
+- For text, sorts them alphabetically.
+
+#### Sorting by Count
+- Groups data by their occurrences and sorts the groups by count.
+- Inside groups with the same count, items are sorted naturally.
+- For numbers, groups are sorted by count, then by value.
+- For text, groups are sorted by count, then alphabetically.
+
+### Examples
+Sort numbers naturally:
+```bash
+python sorting_tool.py -dataType long -sortingType natural -inputFile numbers.txt
+```
+
+Sort words by count:
+```bash
+python sorting_tool.py -dataType word -sortingType byCount -inputFile words.txt -outputFile sorted_words.txt
+```
+
+### Errors and Warnings
+- The tool provides clear error messages for common mistakes, such as missing required parameters or invalid input data.
+
+### Support and Contributions
+Feel free to open an issue or submit a pull request if you find a bug or have suggestions for improvements.
+
+---
 Here's the link to the project: https://hyperskill.org/projects/307
 
 Check out my profile: https://hyperskill.org/profile/103100057
--- Sorting Tool (Python)/task/main.py
+++ Sorting Tool (Python)/task/main.py
@@ -1,10 +1,11 @@
 import argparse
 import sys
 from collections import Counter
+from typing import List, Optional, Union, TextIO
 
 
 class CustomArgumentParser(argparse.ArgumentParser):
-    def error(self, message):
+    def error(self, message: str) -> None:
         if 'expected one argument' in message and '-sortingType' in message:
             print("No sorting type defined!")
             sys.exit()
@@ -16,12 +17,12 @@ class CustomArgumentParser(argparse.ArgumentParser):
             print(f"Error: {message}")
 
 
-def parser():
+def parse_arguments() -> argparse.Namespace:
     parser = CustomArgumentParser()
     parser.add_argument('-dataType', choices=['long', 'line', 'word'])
     parser.add_argument('-sortingType', choices=['byCount', 'natural'])
-    parser.add_argument('-inputFile')
-    parser.add_argument('-outputFile')
+    parser.add_argument('-inputFile', type=argparse.FileType('r'))
+    parser.add_argument('-outputFile', type=argparse.FileType('w'))
     args, unknown = parser.parse_known_args()
 
     if unknown:
@@ -30,12 +31,12 @@ def parser():
     return args
 
 
-def get_input(data_type, file_obj=None):
+def get_input(data_type: str, file_obj: Optional[TextIO] = None) -> List[str]:
     line = file_obj.readline().strip() if file_obj else input()
     return line.split() if data_type in ['long', 'word'] else [line]
 
 
-def process_num(num):
+def process_num(num: str) -> Optional[int]:
     try:
         return int(num)
     except ValueError:
@@ -43,56 +44,55 @@ def process_num(num):
         return None
 
 
-def output_natural(data, data_type, file_obj=None):
-    custom_print(f'Total {data_type}s: {len(data)}', file_obj)
-    sep = '\n' if data_type == 'line' else ' '
-    custom_print('Sorted data:' + sep.join(map(str, sorted(data))), file_obj)
-
-
-def output_by_count(data, data_type, file_obj=None):
-    total_count = sum(count for _, count in data)
-    custom_print(f'Total {data_type}s: {total_count}.', file_obj)
-    for data_tuple in data:
-        custom_print(f'{data_tuple[0]}: {data_tuple[1]} time(s), {int((data_tuple[1] / total_count) * 100)}%.', file_obj)
-
-
-def custom_print(message, file_obj=None):
+def custom_print(message: str, file_obj: Optional[TextIO] = None) -> None:
     if file_obj:
         file_obj.write(message + '\n')
     else:
         print(message)
 
 
-def main():
-    args = parser()
+def output_natural(data: List[Union[int, str]], data_type: str, file_obj: Optional[TextIO] = None) -> None:
+    custom_print(f'Total {data_type}s: {len(data)}', file_obj)
+    sep = '\n' if data_type == 'line' else ' '
+    custom_print('Sorted data:' + sep.join(map(str, sorted(data))), file_obj)
+
+
+def output_by_count(data: List[tuple], data_type: str, file_obj: Optional[TextIO] = None) -> None:
+    total_count = sum(count for _, count in data)
+    custom_print(f'Total {data_type}s: {total_count}.', file_obj)
+    for data_tuple in data:
+        custom_print(f'{data_tuple[0]}: {data_tuple[1]} time(s), {int((data_tuple[1] / total_count) * 100)}%.', file_obj)
+
+
+def main() -> None:
+    args = parse_arguments()
     input_list = []
     data_type = args.dataType or 'word'
     process_func = process_num if data_type == 'long' else lambda x: x
-    output_file = open(args.outputFile, 'w') if args.outputFile else None
 
     if args.inputFile:
-        with open(args.inputFile, 'r') as file_obj:
+        with args.inputFile as file_obj:
             while True:
                 data = get_input(data_type, file_obj)
-                if not data:  # Break the loop if there is no more data
+                if not data:
                     break
-                input_list.extend(process_func(num) for num in data if process_func(num) is not None)
+                input_list.extend(process_func(el) for el in data if process_func(el) is not None)
     else:
         while True:
             try:
                 data = get_input(data_type)
-                input_list.extend(process_func(num) for num in data if process_func(num) is not None)
+                input_list.extend(process_func(el) for el in data if process_func(el) is not None)
             except EOFError:
                 break
 
-    if not args.sortingType or args.sortingType == 'natural':
-        output_natural(input_list, data_type, output_file)
-    else:
+    if args.sortingType == 'byCount':
         counted_input_list = sorted(Counter(input_list).items(), key=lambda x: (x[1], x[0]))
-        output_by_count(counted_input_list, data_type, output_file)
+        output_by_count(counted_input_list, data_type, args.outputFile)
+    else:
+        output_natural(input_list, data_type, args.outputFile)
 
-    if output_file:
-        output_file.close()
+    if args.outputFile:
+        args.outputFile.close()
 
 
 if __name__ == "__main__":
